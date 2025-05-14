f87d506216e181ac06a6e8737f04c53df632f98a
eddyfri <eddy.frighetto@studenti.unipd.it>
2025-05-14 19:19:26 +0200

    minor changes

--- evaluate.py
+++ evaluate.py
@@ -75,7 +75,7 @@ def heuristic_evaluate(env, iterations=1000):
     fruits_eaten_heuristic = []
     wall_hits_heuristic = []
 
-    for iteration in trange(ITERATIONS):
+    for iteration in trange(iterations):
         actions = heuristic_policy(env)
         rewards = env.move(actions)
         wall_hits_count = np.sum(rewards == env.HIT_WALL_REWARD)
@@ -108,62 +108,12 @@ def evaluate(agent, env, iterations=1000):
 
     return rewards_history, wall_hits, fruits_eaten
 
-if __name__ == "__main__":
-    env_ = get_env()
-    GAMMA = .9
-    ITERATIONS = 5000
+def plots(rewards, fruits_eaten, wall_hits):
+    rewards_history_dqn, rewards_history_ddqn, rewards_history_a2c, heuristic_rewards, random_rewards = rewards
+    fruits_eaten_loaded_dqn, fruits_eaten_loaded_ddqn, fruits_eaten_loaded_a2c, fruits_eaten_heuristic = fruits_eaten
+    wall_hits_dqn, wall_hits_ddqn, wall_hits_a2c, wall_hits_heuristic = wall_hits
 
-    agent_dqn = DQNAgent(state_shape=env_.to_state().shape[1:], gamma=GAMMA, n_actions=4, epsilon_start=1., epsilon_end=0.05, epsilon_decay=0.9995, learning_rate=1e-4, target_update_freq=100)
-
-    agent_dqn.load_weights("weights/snake_dqn_weights.h5")
-    agent_dqn.epsilon = agent_dqn.epsilon_end
-
-    rewards_history_dqn, wall_hits_dqn, fruits_eaten_loaded_dqn = evaluate(agent_dqn, env_, iterations=ITERATIONS)
-
-    print(f"Avg Reward - DQN: {np.mean(rewards_history_dqn):.2f}")
-    print(f"Avg Wall Hits - DQN: {np.mean(wall_hits_dqn):.2f}")
-    print(f"Avg Fruits Eaten - DQN: {np.mean(fruits_eaten_loaded_dqn):.2f}")
-
-    env_ = get_env()
-    agent_ddqn = DoubleDQNAgent(state_shape=env_.to_state().shape[1:], gamma=GAMMA, n_actions=4, epsilon_start=1., epsilon_end=0.05, epsilon_decay=0.9995, learning_rate=1e-4, target_update_freq=100)
-    agent_ddqn.load_weights("weights/snake_ddqn_weights.h5")
-    agent_ddqn.epsilon = agent_ddqn.epsilon_end
-    rewards_history_ddqn, wall_hits_ddqn, fruits_eaten_loaded_ddqn = evaluate(agent_ddqn, env_, iterations=ITERATIONS)
-
-    print(f"Avg Reward - DDQN: {np.mean(rewards_history_ddqn):.2f}")
-    print(f"Avg Wall Hits - DDQN: {np.mean(wall_hits_ddqn):.2f}")
-    print(f"Avg Fruits Eaten - DDQN: {np.mean(fruits_eaten_loaded_ddqn):.2f}")
-
-    env_ = get_env()
-    agent_a2c = A2CAgent(state_shape=env_.to_state().shape[1:], gamma=GAMMA, n_actions=4, learning_rate=1e-4, entropy_beta=0.01)
-    agent_a2c.load_weights("weights/snake_a2c_weights.h5")
-
-    rewards_history_a2c, wall_hits_a2c, fruits_eaten_loaded_a2c = evaluate(agent_a2c, env_, iterations=ITERATIONS)
-
-    print(f"Avg Reward - A2C: {np.mean(rewards_history_a2c):.2f}")
-    print(f"Avg Wall Hits - A2C: {np.mean(wall_hits_a2c):.2f}")
-    print(f"Avg Fruits Eaten - A2C: {np.mean(fruits_eaten_loaded_a2c):.2f}")
-
-    # Heuristic evaluation
-    env_ = get_env()
-    heuristic_rewards, wall_hits_heuristic, fruits_eaten_heuristic = heuristic_evaluate(env_, iterations=ITERATIONS)
-
-    print(f"Avg Reward - Heuristic: {np.mean(heuristic_rewards):.2f}")
-    print(f"Avg Wall Hits - Heuristic: {np.mean(wall_hits_heuristic):.2f}")
-    print(f"Avg Fruits Eaten - Heuristic: {np.mean(fruits_eaten_heuristic):.2f}")
-
-    # Random evaluation
-    random_env = get_env()
-    random_rewards = []
-
-    for _ in trange(ITERATIONS):
-        probs = tf.convert_to_tensor([[.25]*4]*random_env.n_boards)
-        #sample actions
-        actions =  tf.random.categorical(tf.math.log(probs), 1, dtype=tf.int32)
-        # MDP update
-        rewards = random_env.move(actions)
-        random_rewards.append(np.mean(rewards))
-
+    # PLOTS
     # Plot average rewards
     plt.figure(figsize=(10, 6))
     plt.bar(['DQN', 'DDQN', 'A2C', 'Heuristic', 'Random'], [np.mean(rewards_history_dqn), np.mean(rewards_history_ddqn), np.mean(rewards_history_a2c), np.mean(heuristic_rewards), np.mean(random_rewards)], color=['blue', 'orange', 'green', 'red', 'purple'])
@@ -193,7 +143,8 @@ if __name__ == "__main__":
 
     # Plot images from training that are in plots folder
     images = os.listdir('plots')
-    images = [img for img in images if img.endswith('.png') and 'average' not in img]
+    images = ["rewards.png", "fruits_eaten.png", "wall_hits.png"]
+    titles = ["Rewards", "Fruits Eaten", "Wall Hits"]
     images.sort()
     fig, axs = plt.subplots(1, len(images), figsize=(15, 5))
     for i, img in enumerate(images):
@@ -201,7 +152,75 @@ if __name__ == "__main__":
         image = plt.imread(img_path)
         axs[i].imshow(image)
         axs[i].axis('off')
-        axs[i].set_title(img.split('.')[0])
+        axs[i].set_title(titles[i])
     plt.tight_layout()
     plt.savefig('plots/training_images.png')
     plt.show()
+
+if __name__ == "__main__":
+    env_ = get_env()
+    GAMMA = .9
+    ITERATIONS = 5000
+    print("--- Evaluating DQN ---")
+    agent_dqn = DQNAgent(state_shape=env_.to_state().shape[1:], gamma=GAMMA, n_actions=4, epsilon_start=1., epsilon_end=0.05, epsilon_decay=0.9995, learning_rate=1e-4, target_update_freq=100)
+
+    agent_dqn.load_weights("weights/snake_dqn_weights.h5")
+    agent_dqn.epsilon = agent_dqn.epsilon_end
+
+    rewards_history_dqn, wall_hits_dqn, fruits_eaten_loaded_dqn = evaluate(agent_dqn, env_, iterations=ITERATIONS)
+
+    print(f"Avg Reward - DQN: {np.mean(rewards_history_dqn):.2f}")
+    print(f"Avg Wall Hits - DQN: {np.mean(wall_hits_dqn):.2f}")
+    print(f"Avg Fruits Eaten - DQN: {np.mean(fruits_eaten_loaded_dqn):.2f}")
+
+    print("\n--- Evaluating DDQN ---")
+    env_ = get_env()
+    agent_ddqn = DoubleDQNAgent(state_shape=env_.to_state().shape[1:], gamma=GAMMA, n_actions=4, epsilon_start=1., epsilon_end=0.05, epsilon_decay=0.9995, learning_rate=1e-4, target_update_freq=100)
+    agent_ddqn.load_weights("weights/snake_ddqn_weights.h5")
+    agent_ddqn.epsilon = agent_ddqn.epsilon_end
+    rewards_history_ddqn, wall_hits_ddqn, fruits_eaten_loaded_ddqn = evaluate(agent_ddqn, env_, iterations=ITERATIONS)
+
+    print(f"Avg Reward - DDQN: {np.mean(rewards_history_ddqn):.2f}")
+    print(f"Avg Wall Hits - DDQN: {np.mean(wall_hits_ddqn):.2f}")
+    print(f"Avg Fruits Eaten - DDQN: {np.mean(fruits_eaten_loaded_ddqn):.2f}")
+
+    print("\n--- Evaluating A2C ---")
+    env_ = get_env()
+    agent_a2c = A2CAgent(state_shape=env_.to_state().shape[1:], gamma=GAMMA, n_actions=4, learning_rate=5e-5, entropy_beta=0.0001)
+    agent_a2c.load_weights("weights/snake_a2c_weights.h5")
+
+    rewards_history_a2c, wall_hits_a2c, fruits_eaten_loaded_a2c = evaluate(agent_a2c, env_, iterations=ITERATIONS)
+
+    print(f"Avg Reward - A2C: {np.mean(rewards_history_a2c):.2f}")
+    print(f"Avg Wall Hits - A2C: {np.mean(wall_hits_a2c):.2f}")
+    print(f"Avg Fruits Eaten - A2C: {np.mean(fruits_eaten_loaded_a2c):.2f}")
+
+    # Heuristic evaluation
+    print("\n--- Evaluating Heuristic ---")
+    env_ = get_env()
+    heuristic_rewards, wall_hits_heuristic, fruits_eaten_heuristic = heuristic_evaluate(env_, iterations=ITERATIONS)
+
+    print(f"Avg Reward - Heuristic: {np.mean(heuristic_rewards):.2f}")
+    print(f"Avg Wall Hits - Heuristic: {np.mean(wall_hits_heuristic):.2f}")
+    print(f"Avg Fruits Eaten - Heuristic: {np.mean(fruits_eaten_heuristic):.2f}")
+
+    # Random evaluation
+    print("\n--- Evaluating Random ---")
+    random_env = get_env()
+    random_rewards = []
+
+    for _ in trange(ITERATIONS):
+        probs = tf.convert_to_tensor([[.25]*4]*random_env.n_boards)
+        #sample actions
+        actions =  tf.random.categorical(tf.math.log(probs), 1, dtype=tf.int32)
+        # MDP update
+        rewards = random_env.move(actions)
+        random_rewards.append(np.mean(rewards))
+
+    print(f"Avg Reward - Random: {np.mean(random_rewards):.2f}")
+
+    rewards = [rewards_history_dqn, rewards_history_ddqn, rewards_history_a2c, heuristic_rewards, random_rewards]
+    fruits_eaten = [fruits_eaten_loaded_dqn, fruits_eaten_loaded_ddqn, fruits_eaten_loaded_a2c, fruits_eaten_heuristic]
+    wall_hits = [wall_hits_dqn, wall_hits_ddqn, wall_hits_a2c, wall_hits_heuristic]
+
+    plots(rewards, fruits_eaten, wall_hits)
